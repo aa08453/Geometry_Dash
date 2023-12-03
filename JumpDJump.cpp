0f382e9a6b5038ad566767b2ae2442113622653a
Ehzem <ehzem.sheikh@gmail.com>
2023-12-03 22:51:29 +0500

    somemoreComments

--- JumpDJump.cpp
+++ JumpDJump.cpp
@@ -1,21 +1,34 @@
 #include "JumpDJump.hpp"
+
 #include <iostream>
 
+// Constructor for the JumpDJump class
 JumpDJump::JumpDJump()
 {
+    // Initialize the source rectangle for texture mapping
     srcRect = {2311, 593, 71, 133};
+    
+    // Initialize the mover rectangle for the JumpDJump object
+    // Positioned at (1400, 370) with a width of 35 and height of 50
     moverRect = {1400, 370, 35, 50};
 }
 
-bool JumpDJump:: collision(Sprite* S) const
+// Function to check collision between the JumpDJump object and a Sprite
+bool JumpDJump::collision(Sprite* S) const
 {
-    int Sprite_front = S->getMoverRect().x;
-    int Sprite_back = S->getMoverRect().x + S->getMoverRect().w;
-    int Sprite_top = S->getMoverRect().y - S->getMoverRect().h;
-    int Sprite_height = S->getMoverRect().y;
-    int Spike_front = moverRect.x;
-    int Spike_back = moverRect.x + moverRect.w;
-    int Spike_height = moverRect.y;
+    // Extract coordinates and dimensions for collision detection
+    int Sprite_front = S->getMoverRect().x; // Sprite's front x-coordinate
+    int Sprite_back = S->getMoverRect().x + S->getMoverRect().w; // Sprite's back x-coordinate + width
+    int Sprite_top = S->getMoverRect().y - S->getMoverRect().h; // Sprite's top y-coordinate
+    int Sprite_height = S->getMoverRect().y; // Sprite's bottom y-coordinate (height)
+    int Spike_front = moverRect.x; // JumpDJump's x-coordinate
+    int Spike_back = moverRect.x + moverRect.w; // JumpDJump's x-coordinate + width
+    int Spike_height = moverRect.y; // JumpDJump's y-coordinate (height)
+
+    // Check if there is a collision between the JumpDJump and the Sprite
     return (((Spike_front <= Sprite_back && Spike_front >= Sprite_front) ||
-    (Spike_back <= Sprite_back && Spike_back >= Sprite_front)) &&(Sprite_height >= Spike_height));
+             (Spike_back <= Sprite_back && Spike_back >= Sprite_front)) &&
+            (Sprite_height >= Spike_height));
 }
+
+
--- JumpDJump2.cpp
+++ JumpDJump2.cpp
@@ -1,19 +1,33 @@
+
 #include "JumpDJump2.hpp"
+
+
 #include <iostream>
 
+// Constructor for the JumpDJump2 class
 JumpDJump2::JumpDJump2()
 {
+    // Initialize the source rectangle for texture mapping
     srcRect = {240, 720, 339, 290};
+    
+    // Initialize the mover rectangle for the JumpDJump2 object
+    // Positioned at (1520, 267) with a width of 250 and height of 105
     moverRect = {1520, 267, 250, 105};
 }
 
+// Function to check collision between the JumpDJump2 object and a Sprite
 bool JumpDJump2::collision(Sprite* S) const
 {
-    int Sprite_front = S->getMoverRect().x;
-    int Sprite_back = S->getMoverRect().x + S->getMoverRect().w;
-    int Sprite_top = S->getMoverRect().y - S->getMoverRect().h;
-    int ceiling2 = moverRect.y;
-    int hanging_front2 = moverRect.x;
-    int hanging_back2 = moverRect.x + moverRect.w;
+    // Extract coordinates and dimensions for collision detection
+    int Sprite_front = S->getMoverRect().x; // Sprite's front x-coordinate
+    int Sprite_back = S->getMoverRect().x + S->getMoverRect().w; // Sprite's back x-coordinate + width
+    int Sprite_top = S->getMoverRect().y - S->getMoverRect().h; // Sprite's top y-coordinate
+    int ceiling2 = moverRect.y; // Ceiling for JumpDJump2
+    int hanging_front2 = moverRect.x; // JumpDJump2's front x-coordinate
+    int hanging_back2 = moverRect.x + moverRect.w; // JumpDJump2's back x-coordinate + width
+
+    // Check if there is a collision between the JumpDJump2 and the Sprite
     return (Sprite_top <= ceiling2 + 35 && Sprite_back <= hanging_back2 && Sprite_front >= hanging_front2);
-}  
+}
+
+
