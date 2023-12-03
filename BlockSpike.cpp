6d1ae87e80bbb7c899209ee64d4654d45ccfa7ba
Ehzem <ehzem.sheikh@gmail.com>
2023-12-03 21:59:08 +0500

    BlockSpikeComment

--- BlockSpike.cpp
+++ BlockSpike.cpp
@@ -1,11 +1,19 @@
-//Purple base with hanging
-
+// Include the header file for the BlockSpike class
 #include "BlockSpike.hpp"
+
+// Include the necessary library for input and output operations
 #include <iostream>
 
+// Constructor for the BlockSpike class
 BlockSpike::BlockSpike()
 {
+    // Initializing the source rectangle for texture mapping
     srcRect = {2311, 593, 71, 133};
+    
+    // Initializing the mover rectangle for the BlockSpike object
+    // The object starts at position (1000, 370) with a size of (35, 50)
     moverRect = {1000, 370, 35, 50};
 }
+
+
 
