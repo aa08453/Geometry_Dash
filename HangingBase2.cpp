5423700d2dddd6b3b10ea74c897283f4bb8130e2
Abdullah Amir <110716135+aa08453@users.noreply.github.com>
2023-12-04 10:44:29 +0500

    Refactor collision detection in HangingBase2.cpp

--- HangingBase2.cpp
+++ HangingBase2.cpp
@@ -11,12 +11,14 @@ HangingBase2::HangingBase2()
 
 bool HangingBase2::collision(Sprite *S) const
 {
-    int Spike_front = moverRect.x;
-    int Spike_back = moverRect.x + moverRect.w;
-    int Sprite_front = S->getMoverRect().x;
-    int Sprite_back = S->getMoverRect().x + S->getMoverRect().w;
-    int Sprite_height = S->getMoverRect().y;
-    int Spike_height = moverRect.y;
-    return (((Spike_front <= Sprite_back && Spike_front >= Sprite_front) ||
-    (Spike_back <= Sprite_back && Spike_back >= Sprite_front)) && (Sprite_height >= Spike_height));
+    // Extract coordinates and dimensions for collision detection
+    int Sprite_front = S->getMoverRect().x;                      // Sprite's front x-coordinate
+    int Sprite_back = S->getMoverRect().x + S->getMoverRect().w; // Sprite's back x-coordinate + width
+    int Sprite_top = S->getMoverRect().y - S->getMoverRect().h;  // Sprite's top y-coordinate
+    int ceiling2 = moverRect.y;                                  // Ceiling for JumpDJump2
+    int hanging_front2 = moverRect.x;                            // JumpDJump2's front x-coordinate
+    int hanging_back2 = moverRect.x + moverRect.w;               // JumpDJump2's back x-coordinate + width
+
+    // Check if there is a collision between the HangingBase2 and the Sprite
+    return (Sprite_top <= ceiling2 + 35 && Sprite_back <= hanging_back2 && Sprite_front >= hanging_front2);
 }
